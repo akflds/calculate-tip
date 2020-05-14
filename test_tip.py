5d7aa76894ed5ea1ba674636fd3f8aea2f25fbdf
akflds <andrewkirsfelds@gmail.com>
2020-05-14 14:55:21 +0100

    Update test_tip.py

--- test_tip.py
+++ test_tip.py
@@ -10,9 +10,8 @@ def test_pytest():
 # Test 1 - check that the function returns the correct value for a 10% tip on a value of 10
 
 
-# Test 2 - check that the function throws a ValueError error for negative values. Test with -200 (negative 200)
+# Test 2 - check that the function throws a ValueError error for negative values. Test with -200 (negative 200) as the amount.
 
 
-# Test 3 - check that the function throws a TypeError error for string input. Use tip ‘ten’ for input
-
+# Test 3 - check that the function throws a TypeError error for string input. Use the string ‘ten’ as input for the tip.
 
