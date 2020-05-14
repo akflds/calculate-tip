870ac331d520126d03ef7f2ad44e0646ee2a3ee1
akflds <andrewkirsfelds@gmail.com>
2020-05-14 13:59:48 +0100

    Create test_tip.py

---
+++ test_tip.py
@@ -0,0 +1,18 @@
+import pytest
+
+from tip import *
+
+def test_pytest():
+    assert True == True
+
+# Create your tests below
+
+# Test 1 - check that the function returns the correct value for a 10% tip on a value of 10
+
+
+# Test 2 - check that the function throws a ValueError error for negative values. Test with -200 (negative 200)
+
+
+# Test 3 - check that the function throws a TypeError error for string input. Use tip ‘ten’ for input
+
+
