6ea47ef5ea9c7b0e8dfd5558f0e666274d423029
Zeellpatel <94588152+Zeellpatel@users.noreply.github.com>
2021-12-03 10:56:08 +1100

    Update test_tip.py

--- test_tip.py
+++ test_tip.py
@@ -8,7 +8,8 @@ def test_pytest():
 # Create your tests below
 
 # Test 1 - check that the function returns the correct value for a 10% tip on a value of 10
-
+def test_10_percent():
+assert calculate_tip(10,10) == 1
 
 # Test 2 - check that the function throws a ValueError error for negative values. Test with -200 (negative 200) as the amount.
 
